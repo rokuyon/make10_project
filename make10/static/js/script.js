8c3951ce68e01e87063132ee9b27ec4f95e8c4cb
Koki Wataoka <contact@wataoka.com>
2019-05-05 17:36:07 +0900

    change: game

--- make10/static/js/script.js
+++ make10/static/js/script.js
@@ -1,6 +1,7 @@
 var formula = [];
 var operator = ["+", "-", "×", "÷"];
 var parentheses = ["(", ")"];
+var figures = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var numbers = [];
 var text;
 
@@ -8,7 +9,7 @@ function getNumber(number){
     numbers.push(number)
 }
 
-function click(e){
+function buttonClick(e){
     //入力が演算子
     if(operator.includes(e.value)){
         if(!operator.includes(formula[formula.length])){
@@ -22,13 +23,13 @@ function click(e){
     else if(e.value == "削除"){
         formula.pop();
     }
-    else if(e.value = "決定"){
+    else if(e.value == "決定"){
         alert(formulaToString + "を送信しました");
     }
     //入力が数字
     else{
         //数式の最後の文字が数字でない時
-        if(!numbers.includes(formula[formula.length])){
+        if(!(figures.includes(formula[formula.length]))){
             formula.push(e.value)
         }
     }
