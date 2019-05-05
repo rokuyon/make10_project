a1461fdfbffe750f69df70b17aabe6168eedf972
murotani <mu.1ok.san@gmail.com>
2019-05-05 21:26:08 +0900

    add: コメントアウトとか

--- make10/static/js/script.js
+++ make10/static/js/script.js
@@ -3,16 +3,16 @@ var operator = ["+", "-", "×", "÷"];
 var parentheses = ["(", ")"];
 var figures = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var numbers = [];
-//var used_numbers = [];
 var text;
 
 function clickFigure(e){
+    // 直前に入力されたものが数値でない時
     if(!(figures.includes(formula[formula.length-1]))){
+        // 既に使った数値かどうかチェック
         for(var i=0; i<numbers.length; i++){
             if(e.value == numbers[i]){
-                formula.push(e.value);
-                //used_numbers.push(e.value);
-                numbers.splice(i, 1);
+                formula.push(e.value);  // 数値を数式に追加
+                numbers.splice(i, 1);   // 使える数値を減らす
                 break;
             }
         }
@@ -22,6 +22,7 @@ function clickFigure(e){
 }
 
 function clickOperator(e){
+    // 直前に入力されたものが演算子でない時
     if(!operator.includes(formula[formula.length-1])){
         formula.push(e.value);
     }
@@ -34,14 +35,23 @@ function clickParenthesis(e){
 }
 
 function popFormula(){
-    var rm = formula.pop();
+    var rm = formula.pop(); // 数式から末尾の文字列を削除
 
+    // 数式から削除したものが数値であった場合
     if(figures.includes(rm)){
-        numbers.push(rm);
+        numbers.push(rm);   // 使用できる数値として復活させる
     }
 
     console.log(numbers);
     document.getElementById("formula").value = formulaToString();
+}
+
+function checkUsedNumber(){
+    // 数値をすべて使ったかチェック
+    if(numbers.length > 0){
+        alert("数字は全部使ってね");
+        return;
+    }
 }
 
 function postFormula(){
