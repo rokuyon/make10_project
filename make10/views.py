1acfff7256a0acc3a9e384665f6529e54e9df21e
Koki Wataoka <contact@wataoka.com>
2019-05-05 15:36:45 +0900

    add: game url

--- make10/views.py
+++ make10/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'make10/index.html')
+
+def game(request):
+    numbers = [1, 2, 3, 4]
+    context = {'numbers': numbers}
+    return render(request, 'make10/game.html', context)
